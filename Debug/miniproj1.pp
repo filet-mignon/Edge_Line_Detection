2a40b1f33e6a0876db39bf26a46430f100d9b40a
Mignon <mignon.ucla@gmail.com>
2016-10-26 16:12:53 -0700

    Made the R_INC and INC values editable at before runtime

--- Debug/miniproj1.pp
+++ Debug/miniproj1.pp
@@ -1,5 +1,33 @@
 # FIXED
 
 miniproj1.obj: ../miniproj1.c
+miniproj1.obj: ../bmp.h
+miniproj1.obj: ../m_mem.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdlib.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdio.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/math.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/float.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/access.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/elfnames.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/mathf.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/unaccess.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/mathl.h
+miniproj1.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/unaccess.h
 
 ../miniproj1.c: 
+../bmp.h: 
+../m_mem.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdlib.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/math.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/float.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/access.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/elfnames.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/mathf.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/unaccess.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/mathl.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/unaccess.h: 
--- Debug/miniproj1_good.pp
+++ Debug/miniproj1_good.pp
@@ -1,33 +1,5 @@
 # FIXED
 
 miniproj1_good.obj: ../miniproj1_good.c
-miniproj1_good.obj: ../bmp.h
-miniproj1_good.obj: ../m_mem.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdlib.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdio.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/math.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/float.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/access.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/elfnames.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/mathf.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/unaccess.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/mathl.h
-miniproj1_good.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/unaccess.h
 
 ../miniproj1_good.c: 
-../bmp.h: 
-../m_mem.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdlib.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/linkage.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdio.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/math.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/float.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/access.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/elfnames.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/mathf.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/unaccess.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/mathl.h: 
-C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/unaccess.h: 
