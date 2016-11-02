534f379bd911b83b600fd0c268b1dcbf37dd66cd
Mignon <mignon.ucla@gmail.com>
2016-11-02 14:14:16 -0700

    Welp I broke it

--- miniproj1.c
+++ miniproj1.c
@@ -4,27 +4,27 @@
 
 #define PI 3.1415926535
 #define INC 15
-#define R_INC 2
-
-#define SOBEL 1
+#define R_INC 1
+#define THRESHOLD 20
 
 unsigned char *bitmap;
 int num_pixels;
+int SOBEL = 0;
 
 //0 is straight up, 180 is straight down
 
 int main(void)
 {
-//	bitmap = imread("C:\\Users\\EE113D\\Desktop\\image1.bmp");
-	bitmap = imread("C:\\Users\\EE113D\\Desktop\\Lab_4\\square.bmp");
+	bitmap = imread("C:\\Users\\EE113D\\Desktop\\image1.bmp");
+//	bitmap = imread("C:\\Users\\EE113D\\Desktop\\Lab_4\\square.bmp");
 
 	int i, j, k, x, y, r;
 	double radius;
 	int result_x, result_y;
 
-	int RANGE = sqrt(WIDTH*WIDTH + HEIGHT*HEIGHT)/R_INC;
 	int WIDTH,HEIGHT;
 	if(SOBEL==0){
+		//If we're processing the original image
 		WIDTH = InfoHeader.Width;
 		HEIGHT = InfoHeader.Height;
 	}
@@ -33,8 +33,8 @@ int main(void)
 		HEIGHT= (InfoHeader.Height-2);
 	}
 
+	int RANGE = sqrt(WIDTH*WIDTH + HEIGHT*HEIGHT)/R_INC;
 	int* hough = (int*)m_malloc(sizeof(int) *2* RANGE * 180/INC);
-
 	for (i = 0; i<(2*RANGE*(180/INC)); i++)
 	{
 		hough[i] = 0;
@@ -53,34 +53,7 @@ int main(void)
 	if(SOBEL==1){
 		sobel_temp = (int*)m_malloc(sizeof(int)*(WIDTH)*(HEIGHT));
 		sobel = (int*)m_malloc(sizeof(int)*(WIDTH-2)*(HEIGHT-2));
-		for(i = 1; i < (InfoHeader.Height-1); i++){
-			for(j = 1; j < (InfoHeader.Width-1); j++){
-				result_x = (bitmap[3*((i-1)*InfoHeader.Width + (j-1))]*sobel_x[2][0] +
-							bitmap[3*((i-1)*InfoHeader.Width + (j))]*sobel_x[2][1] +
-							bitmap[3*((i-1)*InfoHeader.Width + (j+1))]*sobel_x[2][2] +
-							bitmap[3*((i)*InfoHeader.Width + (j-1))]*sobel_x[1][0] +
-							bitmap[3*((i)*InfoHeader.Width + (j))]*sobel_x[1][1] +
-							bitmap[3*((i)*InfoHeader.Width + (j+1))]*sobel_x[1][2] +
-							bitmap[3*((i+1)*InfoHeader.Width + (j-1))]*sobel_x[0][0] +
-							bitmap[3*((i+1)*InfoHeader.Width + (j))]*sobel_x[0][1] +
-							bitmap[3*((i+1)*InfoHeader.Width + (j+1))]*sobel_x[0][2]);
-				sobel_temp[(i-1)*(WIDTH)+(j-1)] = result_x;
-			}
-		}
-		for(i = 2; i < (InfoHeader.Height-2); i++){
-			for(j = 2; j < (InfoHeader.Width-2); j++){
-				result_y = (sobel_temp[((i-1)*WIDTH + (j-1))]*sobel_y[2][0] +
-						sobel_temp[((i-1)*WIDTH + (j))]*sobel_y[2][1] +
-						sobel_temp[((i-1)*WIDTH + (j+1))]*sobel_y[2][2] +
-						sobel_temp[((i)*WIDTH + (j-1))]*sobel_y[1][0] +
-						sobel_temp[((i)*WIDTH + (j))]*sobel_y[1][1] +
-						sobel_temp[((i)*WIDTH + (j+1))]*sobel_y[1][2] +
-						sobel_temp[((i+1)*WIDTH + (j-1))]*sobel_y[0][0] +
-						sobel_temp[((i+1)*WIDTH + (j))]*sobel_y[0][1] +
-						sobel_temp[((i+1)*WIDTH + (j+1))]*sobel_y[0][2]);
-				sobel[(i)*(WIDTH-2)+(j)] = result_y;
-			}
-		}
+
 	}
 
 	if(SOBEL==2){
@@ -89,6 +62,7 @@ int main(void)
 			for(j = 1; j < (InfoHeader.Width-1); j++){
 				result_x = 0;
 				result_y = 0;
+				int a,b;
 				for(a = 0; a < 3; a++){
 					for(b = 0; b < 3; b++){
 						int xn = j + a - 1;
@@ -100,14 +74,28 @@ int main(void)
 					}
 				}
 				sobel[(i-1)*(WIDTH) + (j-1)] = sqrt(result_x*result_x + result_y*result_y);
+
+				if(sobel[(i-1)*(WIDTH) + (j-1)] < 100){
+					sobel[(i-1)*(WIDTH) + (j-1)] = 255;
+				}else{
+					sobel[(i-1)*(WIDTH) + (j-1)] = 0;
+				}
 			}
 		}
+
+		for(i = 0; i < HEIGHT; i++){
+			for(j = 0; j < WIDTH; j++){
+				printf("%d ", (int)sobel[i*WIDTH + j]);
+			}
+			printf("\n");
+		}
+
 	}
 
 	if(SOBEL==0){
 		for(i = 0; i < num_pixels; i++){
 			//Check if black
-			if(bitmap[i*3] <= 0){
+			if(bitmap[i*3] <= 5){
 				//Calculate ze hough transform
 				y = i/InfoHeader.Width;
 				x = i%InfoHeader.Width;
@@ -115,25 +103,40 @@ int main(void)
 				for(theta = 0; theta < 180; theta += INC){
 					radius = y*cos(theta*PI/180) + x*sin(theta*PI/180);
 					r = (int)(radius/R_INC);
-					//printf("x is %d, y is %d, r is %d, theta is %d, ", x, y, r, theta);
+					printf("x is %d, y is %d, r is %d, theta is %d, ", x, y, r, theta);
 					hough[(r+RANGE)*(180/INC) + theta/INC]++;
-					//printf("counter is %d \n", hough[(r+RANGE)*(180/INC) + theta/INC]);
-					//printf(" y index: %f, x index: %f, matrix index: %f \n", (float)(r+RANGE), (float)(theta/INC), (float)((r+RANGE)*(180/INC) + theta/INC));
+					printf("counter is %d \n", hough[(r+RANGE)*(180/INC) + theta/INC]);
+					printf(" y index: %f, x index: %f, matrix index: %f \n", (float)(r+RANGE), (float)(theta/INC), (float)((r+RANGE)*(180/INC) + theta/INC));
 				}
 			}
+		}
+
+		for (j = 0; j<(2*RANGE); j++)
+		{
+			//printf("%d : ", j-RANGE);
+			for(k = 0; k<(180/INC); k++)
+			{
+				printf("%d ", (int)hough[j*(180/INC) + k]);
+				/*
+				if(hough[j*(180/INC) + k] >= THRESHOLD){
+					printf("Radius: %d, Angle: %d, Occurences: %d \n", (j-RANGE)*R_INC, k*INC, hough[j*(180/INC) + k]);
+				}*/
+			}
+			printf("\n");
 		}
 	}
 
 	else{
-		for(i = 0; i < num_pixels; i++){
-			//Check if black
-			if(sobel[i] <= 0){
+		printf("Starting analysis on filtered image...\n");
+		for(i = 0; i < WIDTH*HEIGHT; i++){
+			//Check if white
+			if(sobel[i] == 0){
 				//Calculate ze hough transform
 				y = i/(WIDTH);
 				x = i%(HEIGHT);
 				int theta;
 				for(theta = 0; theta < 180; theta += INC){
-					radius = y*cos(theta*PI/180) + x*sin(theta*PI/180);
+					radius = x*cos(theta*PI/180) + y*sin(theta*PI/180);
 					r = (int)(radius/R_INC);
 					//printf("x is %d, y is %d, r is %d, theta is %d, ", x, y, r, theta);
 					hough[(r+RANGE)*(180/INC) + theta/INC]++;
@@ -144,19 +147,13 @@ int main(void)
 		}
 	}
 
-	for (j = 0; j<(2*RANGE); j++)
-	{
-		//printf("%d : ", j-RANGE);
-		for(k = 0; k<(180/INC); k++)
-		{
-			printf("%d ", hough[j*(180/INC) + k]);
-		}
-		printf("\n");
-	}
+
 
 
 
 	m_free(hough);
+	m_free(sobel);
+	m_free(sobel_temp);
 	return 0;
 }
 
